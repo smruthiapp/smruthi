6b8c9a3528e221b0f9a937d09c151a00d0909312
imradhe <radhesyam.musicals@gmail.com>
2023-06-30 13:12:00 +0530

    Created first unstable version of Smruthi App

---
+++ views/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+    <title>Smruthi App</title>
+    <meta property="og:title" content="Smruthi App" />
+    <?php require('views/partials/head.php'); ?>
+<body>
+    
+</body>
+</html>
