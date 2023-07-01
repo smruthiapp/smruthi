ca45467af5b0223bb846fb2a8ac66000dd27d48b
imradhe <radhesyam.musicals@gmail.com>
2023-07-01 23:00:01 +0530

    Finished home page design

--- views/partials/head.php
+++ views/partials/head.php
@@ -1,7 +1,7 @@
 <head>
 
     <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0,user-scalable=none" />
     <meta name="description" content="<?php echo $config['APP_DESC']; ?>" />
     <meta name="keywords" content="<?php echo $config['APP_KEYWORDS']; ?>" />
     <meta name="mobile-web-app-capable" content="yes" />
@@ -33,6 +33,10 @@
 
     <!-- Jquery -->
     <script src="<?php assets("jquery/jquery.min.js");?>"></script>
+
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="<?php assets('owlcarousel/owl.carousel.min.css');?>">  
+    <script src="<?php assets('owlcarousel/owl.carousel.min.js');?>"></script>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
