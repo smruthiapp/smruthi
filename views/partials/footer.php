c6646a754968c1490de5e0f6b047e34e47003a54
imradhe <radhesyam.musicals@gmail.com>
2023-07-01 22:59:59 +0530

    Finished home page design

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,6 +1,35 @@
-<h4 class="footer mt-5">
-  Developed By
-  <a rel="noopener" href="https://imradhe.com/" target="_blank" style="text-decoration: none;">
-    Radhe Shyam Salopanthula
-  </a>
-</h4>
+<footer class="footer my-5 text-center">
+  <h6>Developed By <strong>Amara Vaani Linguistic Technologies</strong></h6>
+</footer>
+
+<!-- Bottom Navbar -->
+<div class="navbar navbar-fixed-bottom container-fluid">
+        <a href="<?php echo route('')?>"  class="home">
+            <i class="temple-icon"><img src="<?php assets('img/temple.svg');?>" alt="Temple Icon" class="img-fluid"></i>
+            Home
+        </a>
+        <a href="<?php echo route('read')?>"  class="read">
+            <i class="read-icon"><img src="<?php assets('img/read.svg');?>" alt="Read Icon" class="img-fluid"></i>
+            Read
+        </a>
+        <a href="<?php echo route('saved')?>"  class="saved">
+            <i class="saved-icon"><img src="<?php assets('img/saved.svg');?>" alt="Saved Icon" class="img-fluid"></i>
+            Saved
+        </a>
+        <a href="<?php echo route('profile')?>"  class="profile">
+            <i class="saved-icon"><img src="<?php assets('img/profile.svg');?>" alt="Saved Icon" class="img-fluid"></i>
+            Profile
+        </a>
+    
+    </div>
+
+    <script>
+        const home =document.querySelector('.home')
+        const read =document.querySelector('.read')
+        const saved =document.querySelector('.saved')
+        const profile =document.querySelector('.profile')
+        function active(icon){
+          const activeIcon = document.querySelector(icon+" img")
+          activeIcon.src = activeIcon.src.replace('.svg', '-active.svg')
+        }
+    </script>
