24c285a460792865e49e1ac4049b4b8e68a54d2b
imradhe <radhesyam.musicals@gmail.com>
2023-07-01 23:50:31 +0530

    Added partials

---
+++ views/partials/banner.php
@@ -0,0 +1,21 @@
+
+        <div class="d-flex justify-content-center">
+            <div class="banner card rounded bg-smruthi border-0 px-3 py-4" id="randomSloka">
+
+                <div class="d-flex justify-content-around">
+                    <div class="details">
+                        <div class="text-start">
+                            <h2 class="fs-2 text-smruthi-1">Sloka of the Day</h2>
+                            <div class="sloka text-smruthi-white fw-bold fs-7">कोन्वस्मिन्साम्प्रतं लोके गुणवान्कश्च वीर्यवान् ।
+धर्मज्ञश्च कृतज्ञश्च सत्यवाक्यो दृढव्रत:।।1.1.2।।</div>                    
+                            <div class="meaning text-smruthi-1 fs-8">Who in this world lives today endowed with excellent qualities, prowess, righteousness, gratitude, truthfulness and firmness in his vows?</div>
+                        </div>
+                    </div>
+                    <div class="illustration">
+                        <img src="<?php echo assets('img/rama1.svg')?>" alt="Sri Rama" class="img-fluid">
+                    </div>
+                </div>           
+                <a href="<?php echo route('ramayanam/kanda-1-sarga-1-sloka-2');?>" class="btn btn-primary stretched-link opacity-0 p-0 m-0" style="height: 1px">Click Here</a>     
+            </div>
+        </div>
+    
