a6d5ea518801efd77b481f98453dd4212a6c8616
imradhe <radhesyam.musicals@gmail.com>
2023-07-02 16:33:53 +0530

    Updated

---
+++ views/read/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+  <?php 
+    //$config['APP_TITLE'] = "Smruthi website";
+
+    require('views/partials/head.php'); 
+    ?>
+
+  
+  <body>
+      <nav class="navbar">
+        <div class="container-fluid d-flex justify-content-center">            
+          <a class="navbar-brand" href="<?php echo route('')?>"
+          >
+            <img src="<?php echo assets('img/Smruthi.png')?>"
+              alt="Smruthi Logo"
+              class="img-fluid"/>
+          </a>
+        </div>
+      </nav>
+
+    <div class="container" id="read">
+<?php view('partials/reading');?>
+        
+
+        <div class="">
+            <h2 class="fs-4 text-smruthi mt-5 px-2 float">Bhagavad Gita <a href="<?php echo route('read/gita');?>" class="link-smruthi fs-7 fw-bold text-decoration-none float-end">View All</a></h2>
+            
+        </div>
+        
+        <div class="owl-carousel owl-theme mb-5 px-2" id="gita">
+
+        <?php for ($i=1; $i <= 1; $i++) {             
+        ?>
+            
+            <div class="item me-4" id="adhyayam<?php echo $i?>">
+                    <div class="card rounded bg-smruthi-4 border-0 px-2">
+                        <div class="card-body">
+                                                
+                            <div class="details">
+                                <div class="title text-smruthi-black fw-bold fs-7">Arjuna Vishada Yoga</div>
+                                <div class="text-smruthi fw-bold fs-7">47 Slokas</div>
+                            </div>
+                            
+                            <div class="illustration mb-3">
+                                <img src="<?php assets('img/gita'.$i.'.svg')?>" alt="illustration">
+                            </div>
+                        </div>
+                         
+                        <a href="<?php echo route('read/gita/adhyayam/'.$i);?>" class="btn btn-primary stretched-link opacity-0 p-0 m-0" style="height: 1px">Click Here</a>  
+                    </div>
+                </div>
+
+        <?php
+        } ?>
+        </div>
+
+        
+        
+    <script>
+    $('.owl-carousel').owlCarousel({
+        loop:false,
+        responsive: false,
+        autoWidth: true,
+        lazyLoad: true,
+        autoplay: true,
+        autoplayHoverPause: true,
+        startPosition: '#adhyayam18'
+    })
+    </script>
+    <script>
+        truncate('#randomSloka .meaning', 100)
+
+    </script>
+        <?php include('views/partials/footer.php') ?>
+
+    </div>
+  </body>
+</html>
+
+
+    
