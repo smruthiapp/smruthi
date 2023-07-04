aba900b600d0e3764045f9908c66ab4b046c2cc9
imradhe <radhesyam.musicals@gmail.com>
2023-07-04 23:55:38 +0530

    Tested Router

--- views/test.php
+++ views/test.php
@@ -1,2 +1,7 @@
 <?php
-locked();
+// Access the individual variables from $_REQUEST
+echo "<br>Kanda: ".$_REQUEST['kanda'];
+echo "<br>Sarga: ".$_REQUEST['sarga'];
+echo "<br>Sloka: ".$_REQUEST['sloka'];
+//echo json_encode($_REQUEST);
+?>
