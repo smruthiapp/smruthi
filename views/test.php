995b457c00c46bb4d857691487f6b12a27d1c468
imradhe <radhesyam.musicals@gmail.com>
2023-07-05 20:40:48 +0530

    Added testing route

--- views/test.php
+++ views/test.php
@@ -1,7 +1,32 @@
 <?php
-// Access the individual variables from $_REQUEST
-echo "<br>Kanda: ".$_REQUEST['kanda'];
-echo "<br>Sarga: ".$_REQUEST['sarga'];
-echo "<br>Sloka: ".$_REQUEST['sloka'];
-//echo json_encode($_REQUEST);
+
+// Decode JSON
+$data = App::getIndex('gita');
+
+// Print table
+echo '<table>';
+echo '<thead>';
+echo '<tr>';
+echo '<th>Adhyaya</th>';
+echo '<th>Slokas</th>';
+echo '<th>Name</th>';
+echo '<th>Transliteration</th>';
+echo '<th>Description</th>';
+echo '</tr>';
+echo '</thead>';
+echo '<tbody>';
+
+foreach ($data as $adhyaya) {
+    echo '<tr>';
+    echo '<td>' . $adhyaya['adhyaya'] . '</td>';
+    echo '<td>' . $adhyaya['slokas'] . '</td>';
+    echo '<td>' . $adhyaya['name'] . '</td>';
+    echo '<td>' . $adhyaya['transliteration'] . '</td>';
+    echo '<td>' . $adhyaya['description'] . '</td>';
+    echo '</tr>';
+}
+
+echo '</tbody>';
+echo '</table>';
+
 ?>
