e87d686ad52cdd10376984d437441d85cad2a538
imradhe <radhesyam.musicals@gmail.com>
2023-07-06 21:29:41 +0530

    Fixed viewport

--- views/partials/head.php
+++ views/partials/head.php
@@ -1,7 +1,7 @@
 <head>
 
     <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0,user-scalable=none" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0,user-scalable=no" />
     <meta name="description" content="<?php echo $config['APP_DESC']; ?>" />
     <meta name="keywords" content="<?php echo $config['APP_KEYWORDS']; ?>" />
     <meta name="mobile-web-app-capable" content="yes" />
