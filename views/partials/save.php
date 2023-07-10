e5363e9ff3030122d264f552dfb5c9355175150a
imradhe <radhesyam.musicals@gmail.com>
2023-07-10 21:13:18 +0530

    Added save partial

---
+++ views/partials/save.php
@@ -0,0 +1,15 @@
+<div class="offcanvas offcanvas-bottom mx-auto" tabindex="-1" id="saveModal" aria-labelledby="saveModalLabel"
+    data-bs-backdrop="static">
+    <div class="offcanvas-header px-4">
+
+        <h5 class="offcanvas-title" id="saveModalLabel">Save</h5>
+        <button type="button" class="btn-close fs-7" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+
+        <h4 class="text-center fw-bold">
+            Save this sloka
+        </h4>
+
+        </div>
+</div>
