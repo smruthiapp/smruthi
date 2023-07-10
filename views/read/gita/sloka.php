938bc7fb0a0b64186bf403d1c4462509ad616063
imradhe <radhesyam.musicals@gmail.com>
2023-07-10 21:13:38 +0530

    Added sloka

---
+++ views/read/gita/sloka.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+$adhyayas = App::getIndex('gita');
+$adhyaya = $adhyayas[$_REQUEST['adhyaya']-1];
+controller('Gita');
+$gita = new Gita;
+$sloka = $gita->getSloka($_REQUEST['adhyaya'], $_REQUEST['sloka'])[0];
+
+
+$config['APP_TITLE'] = "Bhagavad Gita - Adhyaya ".$sloka['adhyaya'].", Sloka ".$sloka['sloka']." | ". $config['APP_NAME'];
+
+$config['APP_DESC'] = "Read Bhagavad Gita on our Smruthi App now. ".$adhyaya['name']." (".$adhyaya['transliteration'].") - Adhyaya ".$sloka['adhyaya'].", Sloka ".$sloka['sloka'].". ".$adhyaya['description'].".";
+
+
+require('views/partials/head.php');
+?>
+
+<body>
+    <nav class="navbar mx-auto position-fixed fixed-top shadow-sm">
+        <div class="container-fluid mx-2 mt-4 d-block">
+            <a class="link-smruthi-grey text-decoration-none" href="<?php echo route('read/gita/adhyaya/'.$sloka['adhyaya']) ?>">
+            
+                <span class="fs-4">
+                    <i class="bi bi-arrow-left"></i> 
+                </span>
+                <span class="text-smruthi font-smruthi">Bhagavad Gita</span>
+            </a>
+        </div>
+
+        <div class="container-fluid mx-2 mt-3 d-block hideOnScroll">
+            <div class="float-end fs-4 mt-3">
+
+                <a href="#saveModal" class="link-smruthi-grey fw-bold me-3" type="button" data-bs-toggle="offcanvas" data-bs-target="#saveModal" aria-controls="saveModal" aria-label="Share"><i class="bi bi-heart"></i></a>
+
+                <?php include('views/partials/save.php');?>
+
+                <a href="#shareModal" class="link-smruthi-grey fw-bold me-3" type="button" data-bs-toggle="offcanvas" data-bs-target="#shareModal" aria-controls="shareModal" aria-label="Share"><i class="bi bi-share"></i></a>
+
+                <?php include('views/partials/share.php');?>
+                
+            </div>
+
+            <h2 class="text-smruthi fw-bold fs-4"><?php echo $adhyaya['name'];?></h2> 
+            <h2 class="text-smruthi-grey fw-bold fs-6"><?php echo $adhyaya['transliteration'];?></h2>
+            <h3 class="text-muted fs-7">Adhyaya <?php echo $sloka['adhyaya'];?>, Sloka <?php echo $sloka['sloka'];?> </h3>
+        </div>
+        
+    </nav>
+
+    <div class="container" id="gita">
+
+
+        <div class="text-center py-5 px-2" id="gita">
+
+
+        <?php
+                $adhyayas = App::getIndex('gita');
+
+                //print_r(removeEmptyLines($sloka['text']));
+        ?>
+
+            <div class="design">
+                <div class="d1">
+                    <img src="<?php assets('img/design.svg')?>" alt="Design" class="img-fluid">
+                </div>
+            </div>
+
+            <div class="text my-2">
+                <strong class="fs-5">
+                    <?php echo removeEmptyLines($sloka['text']);?>
+                </strong>
+            </div>
+
+            <div class="design">
+                <div class="d2">
+                    <img src="<?php assets('img/design.svg')?>" alt="Design" class="img-fluid">
+                </div>
+            </div>
+
+
+
+            <div class="accordion my-3" id="slokaAccordion">
+
+            <div class="accordion-item border-0 my-3">
+                <h2 class="accordion-header">
+                <button class="accordion-button rounded-pill fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#translation" aria-expanded="true" aria-controls="translation">
+                    Translation
+                </button>
+                </h2>
+                <div id="translation" class="accordion-collapse collapse show">
+                <div class="accordion-body">
+                    <?php echo removeEmptyLines($sloka['translation_en'])?>
+                </div>
+                </div>
+            </div>
+        
+            <div class="accordion-item border-0 my-3">
+                <h2 class="accordion-header">
+                <button class="accordion-button collapsed rounded-pill fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#commentary" aria-expanded="true" aria-controls="commentary">
+                    Commentary
+                </button>
+                </h2>
+                <div id="commentary" class="accordion-collapse collapse">
+                <div class="accordion-body">
+                    <?php echo removeEmptyLines($sloka['commentary_en'])?>
+                </div>
+                </div>
+            </div>
+        
+        </div>
+
+        </div>
+        
+        <div class="bg-smruthi-4 toolbar mx-auto pb-2 hideOnScroll d-flex justify-content-center align-items-center px-2 py-2">
+        <a href="<?php echo route('read/gita/adhyaya/'.$sloka['prev'][0].'/sloka/'.$sloka['prev'][2])?>" class="fs-7 mx-2" <?php echo (empty($sloka['prev'])) ? 'disabled' : '';?>>Prev</a>
+        <a href="#" class="bi bi-play-circle-fill text-smruthi mx-2" id="play"></a>
+        <a href="<?php echo route('read/gita/adhyaya/'.$sloka['next'][0].'/sloka/'.$sloka['next'][2])?>" class="fs-7 mx-2" <?php echo (empty($sloka['next'])) ? 'disabled' : '';?>>Next</a>
+      </div>
+
+
+
+        <?php include('views/partials/footer.php') ?>
+    </div>
+
+</body>
+
+</html>
