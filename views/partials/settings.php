2e89f55b4c2ac33b6eb7df0a27035b54ce18dad2
imradhe <radhesyam.musicals@gmail.com>
2023-07-12 22:56:09 +0530

    Created Settings

---
+++ views/partials/settings.php
@@ -0,0 +1,15 @@
+<div class="offcanvas offcanvas-bottom mx-auto" tabindex="-1" id="settingsModal" aria-labelledby="settingsModalLabel"
+    data-bs-backdrop="static">
+    <div class="offcanvas-header px-4">
+
+        <h5 class="offcanvas-title" id="settingsModalLabel">Settings</h5>
+        <button type="button" class="btn-close fs-7" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+
+        <h4 class="text-center fw-bold">
+            Language Settings
+        </h4>
+
+        </div>
+</div>
