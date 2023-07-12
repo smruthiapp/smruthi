a390cba19c725bc3308b4d09c3db71e245a36b26
imradhe <radhesyam.musicals@gmail.com>
2023-07-12 22:56:14 +0530

    Created Navigate

---
+++ views/partials/navigate.php
@@ -0,0 +1,15 @@
+<div class="offcanvas offcanvas-bottom mx-auto" tabindex="-1" id="navigateModal" aria-labelledby="navigateModalLabel"
+    data-bs-backdrop="static">
+    <div class="offcanvas-header px-4">
+
+        <h5 class="offcanvas-title" id="navigateModalLabel">Navigate</h5>
+        <button type="button" class="btn-close fs-7" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+
+        <h4 class="text-center fw-bold">
+           Navigator
+        </h4>
+
+        </div>
+</div>
