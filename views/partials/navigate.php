b7cb4b7f34e15e92a61c78dfac248f6516a85224
imradhe <radhesyam.musicals@gmail.com>
2024-06-29 11:22:17 +0530

    Modified & Fixed Navigate Modal

--- views/partials/navigate.php
+++ views/partials/navigate.php
@@ -1,75 +1,83 @@
-<div class="offcanvas offcanvas-bottom mx-auto" tabindex="-1" id="navigateModal" aria-labelledby="navigateModalLabel"
-    data-bs-backdrop="static">
+<div class="offcanvas offcanvas-bottom mx-auto" tabindex="-1" id="navigateModal" aria-labelledby="navigateModalLabel">
     <div class="offcanvas-header px-4">
-
         <h5 class="offcanvas-title" id="navigateModalLabel">Navigate</h5>
         <button type="button" class="btn-close fs-7" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
     <div class="offcanvas-body">
-
-        <h4 class="text-center fw-bold">
-
-
-            <div class="text-start">
-                <div class="mb-3">
-                    <label for="adhyaya" class="fs-7">Adhyaya:</label>
-                    <select id="adhyaya" name="adhyaya">
-                        <?php foreach ($adhyayas as $adh): ?>
-                            <option class="fs-7" value="<?php echo $adh['adhyaya']; ?>"><?php echo $adh['name']; ?></option>
-                        <?php endforeach; ?>
-                    </select>
-
-                </div>
-
-                <div>
-
-                    <label for="shloka" class="fs-7">Shloka:</label>
-                    <select id="shloka" disabled>
-                    </select>
-
-                </div>
-
+        <div class="text-center row mt-2 mb-5">
+            <div class="form-floating col-6">
+                <!-- Adhyaya Dropdown-->
+                <select id="adhyaya" name="adhyaya" class="form-select">
+                    <!-- Options will be populated dynamically -->
+                </select>
+                <label for="adhyaya" class="ms-2 fs-7">Adhyaya</label>
             </div>
-
-
-
-        </h4>
-
+            <div class="form-floating col-6" id="sloka-container">
+                <!-- Sloka Dropdown-->
+                <select id="sloka" name="sloka" class="form-select">
+                    <!-- Options will be populated dynamically -->
+                </select>
+                <label for="sloka" class="ms-2 fs-7">Sloka</label>
+            </div>
         </div>
+    </div>
 </div>
 
 <script>
-        $(document).ready(function() {
-            var shlokas = <?php echo json_encode($shlokas); ?>;
-            
-            $('#adhyaya').change(function() {
-                var adhyayaId = $(this).val();
+document.addEventListener('DOMContentLoaded', function() {
+    var adhyayas;
 
-                $('#shloka').empty().append('<option value="">Select Shloka</option>');
-                if (adhyayaId) {
-                    $('#shloka').prop('disabled', false);
-                    shlokas.forEach(function(shloka, index) {
-                        if (shloka['adhyaya'] == adhyayaId) {
-                            $('#shloka').append('<option value="' + (index + 1) + '">' + shloka['text'] + '</option>');
-                        }
-                    });
+    // Fetch the JSON data
+    fetch('<?php echo route("models/gita/index.json") ?>')
+        .then(response => response.json())
+        .then(data => {
+            adhyayas = data;
 
-                } else {
-                    $('#shloka').prop('disabled', true);
-                }
-            });
-
-            $('#shloka').change(function() {
-                var shlokaId = $(this).val();
-                if (shlokaId) {
-                    var adhyayaId = $('#adhyaya').val();
-                    var currentUrl = window.location.href;
-                    var urlParts = currentUrl.split('/');
-                    urlParts[urlParts.length - 1] = shlokaId;
-                    urlParts[urlParts.length - 3] = adhyayaId;
-                    var newUrl = urlParts.join('/');
-                    window.location.href = newUrl;
-                }
+            // Populate the Adhyaya dropdown
+            var adhyayaSelect = document.getElementById('adhyaya');
+            adhyayaSelect.innerHTML = '<option>Select Adhyaya</option>';
+            adhyayas.forEach(adhyaya => {
+                var option = document.createElement('option');
+                option.value = adhyaya.adhyaya;
+                option.text = `${adhyaya.adhyaya} - ${adhyaya.transliteration}`;
+                adhyayaSelect.appendChild(option);
             });
         });
-    </script>
+
+    document.getElementById('adhyaya').addEventListener('change', function() {
+        var adhyayaId = this.value;
+        var slokaSelect = document.getElementById('sloka');
+        var slokaContainer = document.getElementById('sloka-container');
+        slokaSelect.innerHTML = '<option>Select sloka</option>';
+
+        if (adhyayaId) {
+            slokaContainer.style.display = 'block';
+
+            var selectedAdhyaya = adhyayas.find(adhyaya => adhyaya.adhyaya == adhyayaId);
+
+            for (let i = 1; i <= selectedAdhyaya.slokas; i++) {
+                var option = document.createElement('option');
+                option.value = i;
+                option.text = i;
+                slokaSelect.appendChild(option);
+            }
+        } else {
+            slokaContainer.style.display = 'none';
+            slokaSelect.disabled = true;
+        }
+    });
+
+    document.getElementById('sloka').addEventListener('change', function() {
+        var slokaId = this.value;
+        if (slokaId) {
+            var adhyayaId = document.getElementById('adhyaya').value;
+            var currentUrl = window.location.href;
+            var urlParts = currentUrl.split('/');
+            urlParts[urlParts.length - 1] = slokaId;
+            urlParts[urlParts.length - 3] = adhyayaId;
+            var newUrl = urlParts.join('/');
+            window.location.href = newUrl;
+        }
+    });
+});
+</script>
